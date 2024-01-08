a383f9a49d46bd71232d2dc35e3a070286be660b
Emma <polar1183@gmail.com>
2024-01-08 09:00:37 -0600

    Añadiendo imagenes seccion-portafolio

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function scrollToSection(sectionId) {
     targetSection.scrollIntoView({ behavior: 'smooth' });
 }
 
+document.querySelector('.Imagen-portafolio1').addEventListener('mouseenter', function() {
+    this.classList.add('show');
+});
+
+function mostrarVentana() {
+    document.getElementById('ventanaEmergente').style.display = 'flex';
+    console.log("Boton mostrar ventana");
+}
+
+function cerrarVentana() {
+    document.getElementById('ventanaEmergente').style.display = 'none';
+    console.log("Boton cerrar ventana");
+}
